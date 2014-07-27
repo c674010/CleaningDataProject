cef3d580b96faa742aef35ecef9f9b6848c61691
Jie Qin <c674010@hotmail.com>
2014-07-27 17:02:01 -0400

    Cleaning Data Project

---
+++ run_analysis.R
@@ -0,0 +1,27 @@
+setwd("c://JHU//CleaningData")
+
+colTitle <-read.table("features.txt", header=FALSE)
+colTitleName <-colTitle[, "V2"]
+
+train_x <-read.table("X_train.txt", header = FALSE, sep = "")
+train_id <-read.table("subject_train.txt", header = FALSE)
+colnames(train_id) <-"id"
+train_act <-read.table("y_train.txt", header = FALSE)
+colnames(train_act) <-"activity"
+colnames(train_x) <-colTitleName
+train_act$activity <-as.factor(train_act$activity)
+levels(train_act$activity) <-c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
+
+test_x <-read.table("X_test.txt", header = FALSE, sep = "")
+test_id <-read.table("subject_test.txt", header = FALSE)
+colnames(test_id) <-"id"
+test_act <-read.table("y_test.txt", header = FALSE)
+colnames(test_act) <-"activity"
+colnames(test_x) <-colTitleName
+test_act$activity <-as.factor(test_act$activity)
+levels(test_act$activity) <-c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
+
+train <-cbind(train_id, train_act, train_x)
+test <-cbind(test_id, test_act, test_x)
+final <-rbind(train,test)
+write.csv(final, file="project.csv", row.names=FALSE)
